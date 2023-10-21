fbc7083e746bf31510a855a8e85bdcb867ef1f99
Alex <fajartin.alex@gmail.com>
2023-10-21 21:58:55 +0800

    Alex-Update

--- app/src/main/java/com/example/infotech/Dashboard.java
+++ app/src/main/java/com/example/infotech/Dashboard.java
@@ -1,14 +1,65 @@
 package com.example.infotech;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+
 public class Dashboard extends AppCompatActivity {
+
+
+    FloatingActionButton fab;
+    DrawerLayout drawerLayout;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        fab = findViewById(R.id.floatingActionButton);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(""); // Optional: Set the title to an empty string
+        toolbar.getMenu().clear(); // Clear the menu items
+
+
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav  );
+        toggle.syncState();
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/infotech/Notes_Edit.java
@@ -0,0 +1,14 @@
+package com.example.infotech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Notes_Edit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_edit);
+    }
+}
