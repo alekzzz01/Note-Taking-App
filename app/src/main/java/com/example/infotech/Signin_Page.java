0dbd185218f3ff13503fec99ec7f3174d040527b
Jay <jrsaranza92@gmail.com>
2023-10-25 21:32:53 +0800

    as

--- app/src/main/java/com/example/infotech/Signin_Page.java
+++ app/src/main/java/com/example/infotech/Signin_Page.java
@@ -2,28 +2,74 @@ package com.example.infotech;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.infotech.databinding.SigninBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Signin_Page extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private SigninBinding binding;
-
+    FirebaseAuth auth;
+    EditText email, password;
+    Button signin;
+    Handler h = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
+
+        email = findViewById(R.id.logemail);
+        password = findViewById(R.id.logpassword);
+        signin = findViewById(R.id.signinbtn);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emaill = email.getText().toString();
+                String pw = password.getText().toString();
+
+
+                if (TextUtils.isEmpty(emaill)) {
+                    email.setError("Email is required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(pw)) {
+                    password.setError("Password is required");
+                    return;
+                }
+
+                if (password.length() < 7) {
+                    email.setError("Password must be 8 characters long");
+                    return;
+                }
+
+                auth.signInWithEmailAndPassword(emaill, pw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Signin_Page.this, "Logged in Successfuly", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Signin_Page.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
