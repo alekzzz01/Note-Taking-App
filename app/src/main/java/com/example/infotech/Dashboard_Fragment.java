a1992f35d33d93e08f726340b9dab8661ccc7029
basti <bastiandredomingo@gmail.com>
2023-10-25 23:59:31 +0800

    a

--- app/src/main/java/com/example/infotech/Dashboard_Fragment.java
+++ app/src/main/java/com/example/infotech/Dashboard_Fragment.java
@@ -1,11 +1,15 @@
 package com.example.infotech;
 
+import static io.realm.Realm.getApplicationContext;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +17,10 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
 
 
 public class Dashboard_Fragment extends Fragment {
@@ -23,13 +31,17 @@ public class Dashboard_Fragment extends Fragment {
 
     RelativeLayout firstNoteLayout;
 
+    RecyclerView recyclerView;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_dashboard_, container, false);
+        recyclerView = v.findViewById(R.id.recyclerview);
 
-        newNoteButton = v.findViewById(R.id.newnote);
+
+        newNoteButton = v.findViewById(R.id.addnewnotebtn);
 
         newNoteButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +49,21 @@ public class Dashboard_Fragment extends Fragment {
                 // Navigate to Notes_Edit activity
                 Intent a = new Intent(getActivity(), Notes_Edit.class);
                 startActivity(a);
+            }
+        });
+        Realm.init(getApplicationContext());
+        Realm realm = Realm.getDefaultInstance();
+
+        RealmResults<Note> notesList = realm.where(Note.class).findAll();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        MyAdapter myAdapter = new MyAdapter(getApplicationContext(), notesList);
+        recyclerView.setAdapter(myAdapter);
+
+        notesList.addChangeListener(new RealmChangeListener<RealmResults<Note>>() {
+            @Override
+            public void onChange(RealmResults<Note> notes) {
+                myAdapter.notifyDataSetChanged();
             }
         });
 
--- app/src/main/java/com/example/infotech/MainActivity.java
+++ app/src/main/java/com/example/infotech/MainActivity.java
@@ -6,14 +6,18 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.os.Bundle;
 import android.view.View;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,7 +48,22 @@ public class MainActivity extends AppCompatActivity {
        NavigationUI.setupWithNavController(navigationView, navController);
 
 
+        Realm.init(getApplicationContext());
+        Realm realm = Realm.getDefaultInstance();
 
+        RealmResults<Note> notesList = realm.where(Note.class).findAll();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        MyAdapter myAdapter = new MyAdapter(getApplicationContext(), notesList);
+        recyclerView.setAdapter(myAdapter);
+
+        notesList.addChangeListener(new RealmChangeListener<RealmResults<Note>>() {
+            @Override
+            public void onChange(RealmResults<Note> notes) {
+                myAdapter.notifyDataSetChanged();
+            }
+        });
 
     }
 
