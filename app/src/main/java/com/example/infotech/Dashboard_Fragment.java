61ab9f02fdf686bdedd493bb3e5a1e2362db26a3
basti <bastiandredomingo@gmail.com>
2023-10-25 22:03:45 +0800

    a

--- app/src/main/java/com/example/infotech/Dashboard_Fragment.java
+++ app/src/main/java/com/example/infotech/Dashboard_Fragment.java
@@ -1,5 +1,8 @@
 package com.example.infotech;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,26 +10,51 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 public class Dashboard_Fragment extends Fragment {
 
+    ImageButton newNoteButton;
+
+    TextView mainTitleEditText;
+
+    RelativeLayout firstNoteLayout;
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard_, container, false);
+        View v =  inflater.inflate(R.layout.fragment_dashboard_, container, false);
+
+        newNoteButton = v.findViewById(R.id.newnote);
+
+        mainTitleEditText = v.findViewById(R.id.main_title);
+
+        firstNoteLayout = v.findViewById(R.id.first_note);
+
+        newNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to Notes_Edit activity
+                Intent a = new Intent(getActivity(), Notes_Edit.class);
+                startActivity(a);
+            }
+        });
 
 
+        firstNoteLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent b = new Intent(getActivity(), Notes_Edit.class);
+                startActivity(b);
+            }
+        });
 
-
-
-
-
-
-
-
-
+        return v;
 
     }
 }
+
--- app/src/main/java/com/example/infotech/Notes_Edit.java
+++ app/src/main/java/com/example/infotech/Notes_Edit.java
@@ -3,13 +3,26 @@ package com.example.infotech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Notes_Edit extends AppCompatActivity {
+
+    TextView dateTimeTextView;
+
+    Button saveButton;
+    EditText titleEditText, bodyEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,18 @@ public class Notes_Edit extends AppCompatActivity {
 
         ImageButton settingsButton = findViewById(R.id.settingsButton);
         ImageButton backbutton = findViewById(R.id.backButton);
+
+        dateTimeTextView = findViewById(R.id.date_time);
+
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+        String formattedDate = dateFormat.format(date);
+        dateTimeTextView.setText(formattedDate);
+        saveButton = findViewById(R.id.save);
+        titleEditText = findViewById(R.id.title);
+        bodyEditText = findViewById(R.id.body);
+
 
         // Set an onClickListener for the ImageButton
         settingsButton.setOnClickListener(new View.OnClickListener() {
@@ -35,10 +60,16 @@ public class Notes_Edit extends AppCompatActivity {
                         // Handle item click actions here
                         if (item.getItemId() == R.id.move) {
                             // Do something for Item 1
+
+
                         } else if (item.getItemId() == R.id.ChangeColor) {
                             // Do something for Item 2
+
+
                         } else if (item.getItemId() == R.id.Delete) {
                             // Do something for Item 3
+
+
                         }
                         return true;
                     }
@@ -57,5 +88,16 @@ public class Notes_Edit extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(Notes_Edit.this, Dashboard_Fragment.class);
+                startActivity(a);
+            }
+        });
+
+
     }
 }
